c95ab0feba3424c205e4f0956b8b589f2c6fe7fe
Sam0nline <Sam0nline@users.noreply.github.com>
2016-10-21 10:46:53 +0100

    InsertBeginningList

---
+++ SamzList/SamzList/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SamzList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList list = new LinkedList();
+            list.InsertBeginning(16);
+            list.InsertBeginning(32);
+            list.InsertBeginning(64);
+        }
+    }
+}
