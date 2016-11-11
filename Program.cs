6b1660c3d9b8206fcec6754453d9d4b84b2e21d8
Sam0nline <Sam0nline@users.noreply.github.com>
2016-11-11 10:39:45 +0000

    Linked List Algorithms

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SamzList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<int> list = new LinkedList<int>();
+
+            list.InsertBeginning(3);
+            list.InsertAfter(6);
+            list.RemoveBeginning();
+            list.InsertBeginning(5);
+            list.RemoveAfter(2);
+        }
+    }
+}
