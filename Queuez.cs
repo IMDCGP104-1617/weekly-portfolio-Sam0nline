371bce78bbfbad6cb2a4aa0b173c31d2190bf551
Sam0nline <Sam0nline@users.noreply.github.com>
2016-12-02 09:13:36 +0000

    Initial commit
    De-Queue probably doesn't function as intended.
    QLinkedList doesn't present values for the queue.

---
+++ Queuez.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructs
+{
+    class Queuez<T>
+    {
+        private LinkedList<T> queue;
+        private int count;
+
+        public int Count { get { return count; } }
+
+        public Queuez()
+        {
+            queue = new LinkedList<T>();
+        }
+
+        ~Queuez()
+        {
+            while (!IsEmpty())
+            {
+                T output;
+                DeQueue(out output);
+            }
+        }
+
+        public void Enqueue(T data)
+        {
+            count++;
+            queue.InsertBeginning(data);
+        }
+
+        public bool DeQueue(out T output)
+        {
+            output = default(T);
+
+            if (IsEmpty())
+            {
+                return false;
+            }
+            count--;
+            return true;
+        }
+
+        public bool IsEmpty()
+        {
+            return count == 0;
+        }
+
+        public T Peek()
+        {
+            return queue.Head;
+        }
+    }
+}
